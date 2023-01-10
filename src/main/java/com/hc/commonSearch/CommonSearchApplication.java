de89529081feaad8efa4d19197d20123cde28bdd
leeeeeeeeeeehc <ehc0825@naver.com>
2023-01-10 14:12:25 +0900

    first upload
    first upload

---
+++ src/main/java/com/hc/commonSearch/CommonSearchApplication.java
@@ -0,0 +1,13 @@
+package com.hc.commonSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSearchApplication.class, args);
+	}
+
+}
