f016a0d53349f4fa287b391d8f9f3228719ffa0b
ehc0825 <ehc0825@naver.com>
2023-01-12 10:52:39 +0900

    add testCase RegionQueryBuilderTest

---
+++ src/test/java/com/hc/commonsearch/util/region/RegionQueryBuilderTest.java
@@ -0,0 +1,43 @@
+package com.hc.commonsearch.util.region;
+
+import com.hc.commonsearch.config.SearchConfig;
+import com.hc.commonsearch.dto.SearchParam;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+class RegionQueryBuilderTest implements SearchConfig {
+    
+    @Test
+    void testBuildRegionQuery() {
+        boolean success = false;
+        SearchParam searchParam = new SearchParam();
+        searchParam.setBoolQueryBuilder(new BoolQueryBuilder());
+        searchParam.setRegion("지역");
+        searchParam=RegionQueryBuilder.buildRegionQuery(searchParam);
+        if(searchParam.getBoolQueryBuilder().toString().contains(REGION_FIELD)
+                && searchParam.getBoolQueryBuilder().toString().contains(searchParam.getRegion()))
+        {
+            success = true;
+        }
+        assertThat(success).isTrue();
+    }
+    
+    @Test
+    void testBuildRegionQueryWhenRegionIsNull()
+    {
+        boolean success = false;
+        SearchParam searchParam = new SearchParam();
+        searchParam.setBoolQueryBuilder(new BoolQueryBuilder());
+        searchParam=RegionQueryBuilder.buildRegionQuery(searchParam);
+        if(! searchParam.getBoolQueryBuilder().toString().contains(REGION_FIELD))
+        {
+            success = true;
+        }
+        assertThat(success).isTrue();
+    }
+
+}
